f7806b64d1c81859c826f5ef17e62414da03b47e
ant094 <cahyono.ari80@gmail.com>
2020-03-22 20:55:39 +0700

    finish-penyakit

--- Asset/js/storage.js
+++ Asset/js/storage.js
@@ -12,3 +12,57 @@
 
     document.getElementById("date").innerHTML = tanggallengkap;
 
+
+let page1 = document.getElementById('page1');
+let page2 = document.getElementById('page2');
+let page3 = document.getElementById('page3');
+
+
+for (let i = 1; i < 10; i++) {
+    let penyakit = 'penyakit'+i;
+     penyakit = document.getElementById(penyakit);    
+}
+function pageA() {
+    
+    penyakit1.style.display = 'table-row';
+    penyakit2.style.display = 'table-row';
+    penyakit3.style.display = 'table-row';
+    penyakit4.style.display = 'none';
+    penyakit5.style.display = 'none';
+    penyakit6.style.display = 'none';
+    penyakit7.style.display = 'none';
+    penyakit8.style.display = 'none';
+    penyakit9.style.display = 'none';
+}
+function pageB() {
+    penyakit1.style.display = 'none';
+    penyakit2.style.display = 'none';
+    penyakit3.style.display = 'none';
+    penyakit4.style.display = 'table-row';
+    penyakit5.style.display = 'table-row';
+    penyakit6.style.display = 'table-row';
+    penyakit7.style.display = 'none';
+    penyakit8.style.display = 'none';
+    penyakit9.style.display = 'none';
+}
+    function pageC() {
+   
+
+
+        penyakit1.style.display = 'none';
+        penyakit2.style.display = 'none';
+        penyakit3.style.display = 'none';
+        penyakit4.style.display = 'none';
+        penyakit5.style.display = 'none';
+        penyakit6.style.display = 'none';
+        penyakit7.style.display = 'table-row';
+        penyakit8.style.display = 'table-row';
+        penyakit9.style.display = 'table-row';
+    }
+
+   
+
+page1.addEventListener("click", pageA);
+page2.addEventListener("click", pageB);
+page3.addEventListener("click", pageC);
+
